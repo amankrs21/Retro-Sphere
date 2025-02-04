4d4a7bea778ca6d14aaad180ebb1f4a84bc62371
Aman Singh <amankrs212@gmail.com>
2025-02-05 03:52:43 +0530

    FEAT: enhance createGroup function to validate group name and add members; implement fetchGroups function

--- server/src/controller/group.controller.mjs
+++ server/src/controller/group.controller.mjs
@@ -5,15 +5,68 @@ import GroupModel from "../models/group.model.mjs";
 const createGroup = async (req, res, next) => {
     try {
         const { name, members } = req.body;
+
+        // check if group name already exists
+        const groupExist = await GroupModel.findOne({ name });
+        if (groupExist) {
+            return res.status(400).json({
+                message: "Group name already exists",
+            });
+        }
         const group = new GroupModel({
             name,
-            members,
-            status: 'active',
-            createdBy: req.user._id,
+            status: "active",
+            createdBy: req.userId,
         });
         await group.save();
-        res.status(201).json({ message: "Group created successfully!" });
+
+        // find members id from database
+        const memberNotFound = [];
+        const membersId = [req.userId];
+        for (let i = 0; i < members.length; i++) {
+            const member = members[i];
+            const memberExist = await UserModel.findOne({ email: member });
+            if (!memberExist) {
+                memberNotFound.push(member);
+            } else {
+                membersId.push(memberExist._id);
+            }
+        }
+
+        // add members to group
+        await MemberModel.insertMany(
+            membersId.map((memberId) => ({
+                user: memberId,
+                status: "active",
+                group: group._id,
+            }))
+        );
+
+        return res.status(201).json({
+            message: "Group created successfully",
+            group,
+            memberNotFound,
+        });
     } catch (error) {
         next(error);
     }
 };
+
+
+
+// fetch groupd and their members
+const fetchGroups = async (req, res, next) => {
+    try {
+        const groups = await GroupModel.find({ createdBy: req.userId }).populate(
+            "members"
+        );
+        return res.status(200).json({
+            groups,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+// exporting functions
+export { createGroup, fetchGroups };
